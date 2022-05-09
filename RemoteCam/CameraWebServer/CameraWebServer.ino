9f63d040533df268f5fae1364ebcdc37c59d106c
Steve-Fisher <steve@wayupnorth.co.uk>
2022-05-09 10:30:24 +0100

    Striped down CameraWebServer.  Working.

--- RemoteCam/CameraWebServer/CameraWebServer.ino
+++ RemoteCam/CameraWebServer/CameraWebServer.ino
@@ -50,12 +50,7 @@ void setup() {
   }
 
   sensor_t * s = esp_camera_sensor_get();
-  // initial sensors are flipped vertically and colors are a bit saturated
-  if (s->id.PID == OV3660_PID) {
-    s->set_vflip(s, 1); // flip it back
-    s->set_brightness(s, 1); // up the brightness just a bit
-    s->set_saturation(s, -2); // lower the saturation
-  }
+  
   // drop down frame size for higher initial frame rate
   s->set_framesize(s, FRAMESIZE_QVGA);
 
